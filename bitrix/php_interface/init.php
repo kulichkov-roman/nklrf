af90444af6093e5f54397115f735a237020c7a41
Kulichkov Roman <roman@kulichkov.pro>
2016-04-13 01:24:38 +0600

    Удалил /bitrix/php_interface, весь проект в /local/

--- bitrix/php_interface/init.php
+++
@@ -1,4 +0,0 @@
-<?
-if(file_exists($_SERVER["DOCUMENT_ROOT"]. "/bitrix/php_interface/itconstruct/eladdev.php"))
-	require_once $_SERVER["DOCUMENT_ROOT"]. "/bitrix/php_interface/itconstruct/eladdev.php";
-?>
