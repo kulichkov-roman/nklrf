596cce49684de69e55c56ecec9e6470326f98ade
Roman Kulichkov <roman@kulichkov.pro>
2016-04-13 13:38:41 +0600

    Синхронизировал проект на work

--- bitrix/templates/.default/components/bitrix/news/postr_sv/bitrix/news.detail/.default/component_epilog.php
+++
@@ -1,6 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-
-<?
-$APPLICATION->SetPageProperty("description", $arResult["NAME"]);
-$APPLICATION->SetPageProperty("title", $arResult["NAME"]);
-?>
